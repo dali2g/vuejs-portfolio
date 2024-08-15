eb64d0766b289a925db547633306a2403958ffba
dali2g <mamejdi12@gmail.com>
2024-08-15 02:33:24 +0200

    first commit

--- pages/projects.vue
+++ pages/projects.vue
@@ -80,7 +80,7 @@ import { ref } from 'vue'
 
 const config = useRuntimeConfig()
 
-const techs = ['React', 'HTML', 'CSS', 'Vue', 'Angular', 'Gatsby', 'Flutter']
+const techs = ['React','Next', 'Express', 'Figma', 'Langchain','FastAPI']
 const filters = ref(['all'])
 const showFilters = ref(true)
 const projects = ref(config.public.dev.projects)
